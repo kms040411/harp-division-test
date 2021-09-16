81506f25d93ffcb0c20f89d06fbb7fe130320278
Minsu Kim <mskim@casys.kaist.ac.kr>
2021-09-16 13:27:03 +0900

    fix 4

--- testbench/multiply/hw/multiplier_tb.sv
+++ testbench/multiply/hw/multiplier_tb.sv
@@ -103,7 +103,7 @@ module app_afu(
 
     // DUT
     localparam DATA_LEN = 32;
-    localparam PIPELINE_STAGE = 2;
+    localparam PIPELINE_STAGE = 0;
 
     logic d_reset;
     logic [DATA_LEN-1:0] d_a;
